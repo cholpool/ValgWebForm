247cf6878e5562d5ee5cd0fc9e6116941e287052
cholpool <117321445+cholpool@users.noreply.github.com>
2023-09-19 13:00:47 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="StemmeLabel" runat="server" Text="Stem her"></asp:Label>
+        <br />
+
+        <asp:Label ID="KommuneLabel" runat="server" Text="Kommune :"></asp:Label>
+
+        <br />
+
+        <asp:DropDownList ID="DropDownListKommu" runat="server">
+            <asp:ListItem Selected="True" Value="0">Velg kommune</asp:ListItem>
+            </asp:DropDownList>
+
+        <br />
+
+        <asp:Label ID="PartiLabel" runat="server" Text="Parti :"></asp:Label>
+
+        <br />
+
+        <asp:DropDownList ID="DropDownListParti" runat="server">
+            <asp:ListItem Selected="True" Value="0">Velg Party</asp:ListItem>
+        </asp:DropDownList>
+
+        <br /><br />
+
+        <asp:Button ID="StemButton" runat="server" Text="Stem" OnClick="StemButton_Click" />
+
+        <br /><br />
+
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>        
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetAllVote();
+            if (!Page.IsPostBack)//new request page, not triggered on button click - postback
+            {
+                BindDropDownListParti();
+                BindDropDownListKommu();
+            }
+        }
+
+        private void BindDropDownListParti()//dette blir en select *. det som returneres skal bindes til dropdown 
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * from Parti", conn);//@ betyr at det er et parameter
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+
+            //loope gjennom datatable for å hente ut partinavn. lage et dropdownitem og putte navnet i det
+            foreach (DataRow row in dt.Rows)
+            {
+                ListItem item = new ListItem(row["Parti"].ToString(), row["Pid"].ToString());//hente ut verdier
+                DropDownListParti.Items.Add(item);//legge item i lista
+            }
+
+            //DropDownListParti.DataSource= dt;
+            DropDownListParti.DataBind();
+        }
+
+        private void BindDropDownListKommu()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * from Kommune", conn);//@ betyr at det er et parameter
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+
+            //loope gjennom datatable for å hente ut partinavn. lage et dropdownitem og putte navnet i det
+            foreach (DataRow row in dt.Rows)
+            {
+                ListItem item = new ListItem(row["Kommune"].ToString(), row["Kid"].ToString());//hente ut verdier
+                DropDownListKommu.Items.Add(item);//legge item i lista
+            }
+
+            //DropDownListParti.DataSource= dt;
+            DropDownListKommu.DataBind();
+        }
+
+        private void GetAllVote()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from Vote", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void StemButton_Click(object sender, EventArgs e)
+        {
+            SqlParameter param;
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO Vote (Pid, Kid) VALUES (@Pid, @Kid)", conn);
+                cmd.CommandType = CommandType.Text;
+
+                param = new SqlParameter("@Pid", SqlDbType.Int);
+                param.Value = int.Parse(DropDownListParti.SelectedValue);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@Kid", SqlDbType.Int);
+                param.Value = int.Parse(DropDownListKommu.SelectedValue);
+                cmd.Parameters.Add(param);
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                GetAllVote();
+            }
+        }
+
+    }
+
+       
+    
+}
